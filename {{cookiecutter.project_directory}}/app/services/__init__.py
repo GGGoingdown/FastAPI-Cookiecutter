0c61e825c412277168439c4aab893422e2e39a39
GGGoingdown <eddiepai0401@gmail.com>
2022-08-03 14:30:13 +0800

    Fix rebase conflict

--- {{cookiecutter.project_directory}}/app/services/__init__.py
+++ {{cookiecutter.project_directory}}/app/services/__init__.py
@@ -1,8 +1,8 @@
 from .log import LoggerInitialize  # noqa: F401
 from .auth import (  # noqa: F401
-    AuthenticationService,  # noqa: F401
-    AuthorizationService,  # noqa: F401
-    AuthenticationSelector,  # noqa: F401
-    JWTHandler,  # noqa: F401
-)  # noqa: F401
+    AuthenticationService,
+    AuthorizationService,
+    AuthenticationSelector,
+    JWTHandler,
+)
 from .user import UserService  # noqa: F401
