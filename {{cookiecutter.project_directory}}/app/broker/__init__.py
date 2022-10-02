489806bc94a5f31ac2e79ade9f435e1d08840ac6
GGGoingdown <eddiepai0401@gmail.com>
2022-10-02 11:08:35 +0800

    Update broker worker init connect

--- {{cookiecutter.project_directory}}/app/broker/__init__.py
+++ {{cookiecutter.project_directory}}/app/broker/__init__.py
@@ -1,28 +1,8 @@
 import celery
-import sentry_sdk
 from celery import current_app as current_celery_app
-from celery.signals import worker_process_init
-from sentry_sdk.integrations.celery import CeleryIntegration
-from typing import Any
-from loguru import logger
 
 ###
-from app import __VERSION__
-from app.config import CeleryConfiguration, settings
-
-
-# Inititalize
-@worker_process_init.connect
-def worker_initialize(*args: Any, **kwargs: Any) -> None:
-    logger.info("--- Worker initialize ...")
-    if settings.sentry.dns:
-        sentry_sdk.init(
-            settings.sentry.dns,
-            traces_sample_rate=settings.sentry.trace_sample_rates,
-            release=f"{settings.app.application_name}-worker@{__VERSION__}",
-            environment=settings.app.env_mode.value,
-            integrations=[CeleryIntegration()],
-        )
+from app.config import CeleryConfiguration
 
 
 def create_celery() -> celery:
--- {{cookiecutter.project_directory}}/app/broker/tasks.py
+++ {{cookiecutter.project_directory}}/app/broker/tasks.py
@@ -1,13 +1,30 @@
-from typing import Dict
+import sentry_sdk
+from typing import Dict, Any
+from loguru import logger
+from celery.signals import worker_process_init
+from sentry_sdk.integrations.celery import CeleryIntegration
 
 # from loguru import logger
 from celery import shared_task
 
-# from dependency_injector.wiring import inject, Provide
 
 ###
-# from app import services
-# from app.containers import Application
+from app import __VERSION__
+from app.config import settings
+
+# Inititalize
+@worker_process_init.connect
+def worker_initialize(*args: Any, **kwargs: Any) -> None:
+    logger.info("--- Worker initialize ...")
+    if settings.sentry.dns:
+        sentry_sdk.init(
+            settings.sentry.dns,
+            traces_sample_rate=settings.sentry.trace_sample_rates,
+            release=f"{settings.app.application_name}-worker@{__VERSION__}",
+            environment=settings.app.env_mode.value,
+            integrations=[CeleryIntegration()],
+        )
+
 
 #################################################################################
 #                   Health Check
